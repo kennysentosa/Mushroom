66c1d13076f52de2c7e10b6dd990fd16fea32c6a
Kennysentosa <Kennysentosajaya@gmail.com>
2023-09-23 16:48:37 +0700

    Update gambar

--- src/Scenes/MushroomFighter.js
+++ src/Scenes/MushroomFighter.js
@@ -10,31 +10,38 @@ export default class MushroomFighter extends Phaser.Scene{
         this.halfheight = this.scale.height / 2
 
         this.player = undefined
-        this.speed = 1000
+        this.speed = 100
         this.cursors = this.input.keyboard.createCursorKeys();
 
+        this.healthValue = 100
+        this.healthDisplay = undefined
     }
 
     preload() {
         this.load.spritesheet("Mushroom", "Assets/Mushroom.png", {
-            frameWidth: 88,
-            frameHeight: 106,
+            frameWidth: 29.3,
+            frameHeight: 35.3,
         })
         this.load.image("Bg", "Assets/BG700.png")
+        //UI
+        this.load.image("UIbelow", "Assets/UIbelow.png")
+        this.load.image("UIbelowBG", "Assets/UIbelowBG.png")
     }
     
     create() {
         this.add.image(this.halfheight, this.halfwidth, "Bg");
+        this.add.image(this.halfwidth, 624, "UIbelowBG") //UI
+        // this.healthDisplay = this.add.rectangle(123, 631, 166, 133, d40d3e)
+        this.Stats("Health", 0)
+        this.Stats("EXP", 338)
+        // this.add.image(this.halfwidth, 624, "UIbelow") //UI
         this.player = this.createplayer();
         this.cameras.main.startFollow(this.player)
-        this.cameras.setSize(100,100)
-
+        this.cursors = this.input.keyboard.createCursorKeys()
     }
 
     update(time) {
         this.moveplayer(this.player, time)
-
-
     }
 
     createplayer() {
@@ -69,28 +76,34 @@ export default class MushroomFighter extends Phaser.Scene{
         })
         
         return player
-        
     }
 
     moveplayer(player, time) {
         if (this.cursors.left.isDown) {
-            this.player.setVelocity(-this.speed, 0);
+            this.player.setVelocityX(this.speed * -1);
             this.player.anims.play("Left", true);
-
         } else if (this.cursors.right.isDown) {
-            this.player.setVelocity(this.speed, 0);
+            this.player.setVelocityX(this.speed);
             this.player.anims.play("Right", true);
-
         } else if (this.cursors.up.isDown) {
-            this.player.setVelocity(0, -this.speed);
+            this.player.setVelocityY(this.speed * -1);
             this.player.anims.play("Up", true);
-
         } else if (this.cursors.down.isDown) {
-            this.player.setVelocity(0, this.speed);
+            this.player.setVelocityY(this.speed);
             this.player.anims.play("Down", true);
         } else {
             this.player.setVelocity(0, 0);
             this.player.anims.play("Standby", true);
         }
+    }
+
+    Stats(Stat, Value) {
+        if (Stat == "Health") {
+            this.add.rectangle(123, 631, 166, 133 + Value/133, 0xa85232)
+        } else if (Stat == "EXP") {
+            this.add.rectangle(349 , 579, 0 + Value/338 * 338, 42, 0x22baa3 )
+        }
+
+
     }
 }
